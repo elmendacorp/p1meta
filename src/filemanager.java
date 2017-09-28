bcb01855c978dde230d59543470d5ea7cdcf5166
franruizlopez <elmendacorp@gmail.com>
2017-09-28 18:54:06 +0200

    añadidos datos

--- src/filemanager.java
+++ src/filemanager.java
@@ -1,2 +1,10 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class filemanager {
+    File archivo;
+    FileReader reader = null;
+    BufferedReader bf;
 }
