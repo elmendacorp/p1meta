baa892ddbb6974898ba1d7e0e084d7afc4683a02
franruizlopez <elmendacorp@gmail.com>
2017-09-28 19:16:24 +0200

    clase transmisor

---
+++ src/Transmisor.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Transmisor {
+    static int id;
+    private static int frecuencias [];
+    public static void Transmisor(int valor){
+        id = valor;
+    }
+
+}
--- src/filemanager.java
+++ src/filemanager.java
@@ -2,9 +2,29 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.security.PublicKey;
 
 public class filemanager {
-    File archivo;
-    FileReader reader = null;
-    BufferedReader bf;
+    public static void LeeNodos(String nombreFichero) throws IOException{
+        File archivo;
+        FileReader reader = null;
+        BufferedReader bf;
+
+        try {
+            archivo= new File(nombreFichero);
+            reader = new FileReader(archivo);
+            bf = new BufferedReader(reader);
+            String lectura;
+            String [] datos;
+            lectura= bf.readLine();
+            datos = lectura.split(" ");
+
+
+        }catch (Exception e){}
+
+    }
+    public static void LeeFrecuencias(String nombreFichero) throws IOException{}
+    public static void LeePuntuaciones(String nombreFichero) throws IOException{}
+
+
 }
