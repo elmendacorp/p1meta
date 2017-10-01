5d3cd2b3935ae1f1469a066b04ac1e091e0cebb3
Rafael <rmr00034@red.ujaen.es>
2017-10-01 17:10:23 +0200

    Arreglado leeTransmisores

--- src/Filemanager.java
+++ src/Filemanager.java
@@ -1,34 +1,47 @@
+import javafx.util.Pair;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Filemanager {
 
-    public static void leeTransmisores(String nombreFichero) throws IOException{
-        File archivo;
-        FileReader reader = null;
-        BufferedReader bf;
+    private List<Pair<Integer,Integer>> transmisores;
 
-        ArrayList<Transmisor> milista= new ArrayList<Transmisor>();
+    public Filemanager(){
+        transmisores = new ArrayList<>();
+    }
+
+    public void leeTransmisores(String nombreFichero){
+
         try {
-            archivo= new File(nombreFichero);
-            reader = new FileReader(archivo);
-            bf = new BufferedReader(reader);
-            String lectura;
-            String [] datos;
-            lectura= bf.readLine();
-            Transmisor tr;
+            FileInputStream fstream = new FileInputStream(nombreFichero);
+            BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
 
-            while(lectura!=null){
-                datos = lectura.split("\\s+");
-                tr = new Transmisor(Integer.parseInt(datos[1]),Integer.parseInt(datos[2]));
-                milista.add(tr);
-                lectura= bf.readLine();
+            String strLine = br.readLine();
+
+            while (strLine != null && strLine.length() != 1) {
+
+                String[] cadena;
+
+                cadena = strLine.split("\\s+");
+                Integer transmisor = Integer.parseInt(cadena[1]);
+                Integer banda = Integer.parseInt(cadena[2]);
+
+                Pair<Integer, Integer> par = new Pair<>(transmisor, banda);
+                transmisores.add(par);
+
+                strLine = br.readLine();
             }
 
-        }catch (Exception e){}
+            br.close();
+        }catch (Exception e){
+            System.out.println ("Fallo al leer el fichero de transmisores: " + e);
+        }
 
     }
     public static void leeFrecuencias(String nombreFichero) throws IOException{
--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,8 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String [ ] args){
-        System.out.println("-----------------------------------");
-        Filemanager fmanager = new Filemanager();
-        try {
-            fmanager.leeTransmisores("/archivos_guion/instancias/Ejemplo/var.txt");
-        } catch (IOException e) {
-            System.out.println("Reventando por doquier");
-            e.printStackTrace();
-        }
+        Filemanager filemanager = new Filemanager();
 
-
+        filemanager.leeTransmisores("./archivos_guion/instancias/Ejemplo/var.txt");
     }
 }
