c42b7fcf404d8aa1a581e08fc6e73307b7f24bdb
franruizlopez <elmendacorp@gmail.com>
2017-10-03 13:17:19 +0200

    Nombres de autores

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,9 @@
+/**
+ * @Author Rafael Martinez Rubio
+ * @Mail rmr00034@red.ujaen.es
+ * @Author Francisco Jesus Ruiz Lopez
+ * @Mail fjrl0016@red.ujaen.es
+ */
 
 public class Main {
     public static void main(String [ ] args){
