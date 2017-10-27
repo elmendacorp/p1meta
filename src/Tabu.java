fed684d55da3d5226a240f8e2e1db1b658ee86da
Rafael <rmr00034@red.ujaen.es>
2017-10-27 14:19:58 +0200

    Continuacion de BT

--- src/Tabu.java
+++ src/Tabu.java
@@ -16,32 +16,38 @@ public class Tabu {
         listaTabu = new HashMap<>();
         rd = new Random();
         rd.setSeed(semilla);
-        direccion = Math.abs(rd.nextInt(2));
-
+        direccion = rd.nextInt(2);
     }
 
     public void generaSolucion() {
-        int frecInicio = 0;
-        int iteraciones = 0;
+        direccion = rd.nextInt(2);
         int size = datos.getTransmisores().size();
+        int iteraciones = 0;
+        int iterSinMejora = 0;
         int idTransmisor = 0;
         int rango = 0;
-        ArrayList<CosteFrecuencia> listaFrecuencias = new ArrayList<>();
         int posFrRandom = 0;
+        ArrayList<CosteFrecuencia> listaFrecuencias = new ArrayList<>();
 
         while (iteraciones < 10000) {
 
             //Comprueba que no has llegado al estancamiento
-
             // si has llegado realiza la reinicializacion copiando lo que tengas en la estuctura de celta tabu
+            if (iterSinMejora == 2000) {
+                iterSinMejora = 0;
+
+            }
 
             ++iteraciones;
-            direccion = Math.abs(rd.nextInt(2));
+            ++iterSinMejora;
+            direccion = rd.nextInt(2);
+            posFrRandom = rd.nextInt(datos.getFrecuencias().get(rango).getFrecuencias().size());
             listaFrecuencias.clear();
 
             idTransmisor = datos.getTransmisores().get(rd.nextInt(size)).getId();
             rango = datos.getTransmisores().get(idTransmisor).getRango();
-            listaFrecuencias = copiaFrecuencias(rango);
+
+            listaFrecuencias = copiaFrecuencias(rango, posFrRandom, direccion);
             // calcula las 20 frecuencias asociadas a un transmisor
 
             // calcula el coste por cada frecuencia
@@ -49,10 +55,6 @@ public class Tabu {
             // coge la mejor y decide si vas a meterla en las soluciones
 
             // añade la frecuencia a la estructuda de celtatabu para mantener los transmisores con las mejores frecuencias y sus apariciones
-
-
-
-            posFrRandom = rd.nextInt(frecuencias.size());
 
             ArrayList<CosteFrecuencia> costesfrecuencias = new ArrayList<>();
 
@@ -71,21 +73,21 @@ public class Tabu {
 
         if (frecuencias.size() <= 20) {
             for (Integer fr : datos.getFrecuencias().get(rango).getFrecuencias()) {
-                finalList.add(new CosteFrecuencia(fr,0));
+                finalList.add(new CosteFrecuencia(fr, 0));
             }
 
         } else {
 
             if (direccion == 0) {
                 for (int i = posInicial; finalList.size() < 20; --i) {
-                    finalList.add(new CosteFrecuencia(frecuencias.get(i),0));
+                    finalList.add(new CosteFrecuencia(frecuencias.get(i), 0));
                     if (i == 0) {
                         i = frecuencias.size() - 1;
                     }
                 }
             } else {
                 for (int i = posInicial; finalList.size() < 20; ++i) {
-                    finalList.add(new CosteFrecuencia(frecuencias.get(i),0));
+                    finalList.add(new CosteFrecuencia(frecuencias.get(i), 0));
                     if (i == frecuencias.size() - 1) {
                         i = 0;
                     }
